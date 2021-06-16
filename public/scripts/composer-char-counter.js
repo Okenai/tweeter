d6fd1cd5f0cb2350ba5ab687861957f4f875e3ee
Lana <kassembayeva@gmail.com>
2021-06-16 18:56:42 +0000

    the function is written to count the key events and change class to red after the limit is reached

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  $('textarea').on('keyup', function(){
+    $('.counter').html(140 - $('textarea').val().length);
+    
+    if($('.counter').html() <0 ) {
+    $('.counter').addClass('exceeded')
+    } else {
+      $('.counter').removeClass('exceeded')
+    }
+   
+  })
+  
+});

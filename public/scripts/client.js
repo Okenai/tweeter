8c0b7bc30963804ca847a610af80e12ffa2617f3
Lana <kassembayeva@gmail.com>
2021-06-17 03:25:56 +0000

    the function rendering the database into new tweet article is defined

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,29 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const createTweetElement = (tweetData) => {
+  const $tweet = $("<article>").addClass("tweet")
+  let html = `<header>
+    <div class="name"> <img class="avatars" src="${tweetData.user.avatars}">${tweetData.user.name}</div>
+    <div class="handle">${tweetData.user.handle}</div>
+  </header>
+  <p class="content">${tweetData.content.text}</p>
+  <footer>
+    <div class="date-stamp">${timeago.format(tweetData.created_at)}</div>
+    <div>
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>
+    </div>
+  </footer>`
+  const newTweet = $tweet.append(html)
+  return newTweet;
+}
+
+const renderTweets = (tweets) => {
+  let tweetsContainer = $('.tweet-container').html('');
+  tweets.forEach((tweet) => {
+    let newTweet = createTweetElement(tweet);
+    tweetsContainer.prepend(newTweet);
+  });
+}

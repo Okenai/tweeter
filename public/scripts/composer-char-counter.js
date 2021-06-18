6c7cd00390f99eb983b4c967f41315f745edc719
Lana <kassembayeva@gmail.com>
2021-06-18 07:17:48 +0000

    updated files

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,10 @@
-$(document).ready(function() {
-
-  $('textarea').on('keyup', function(){
+$(document).ready(function () {
+  $('textarea').on('keyup', function () {
     $('.counter').html(140 - $('textarea').val().length);
-    
-    if($('.counter').html() <0 ) {
-    $('.counter').addClass('exceeded')
+    if ($('.counter').html() < 0) {
+      $('.counter').addClass('exceeded');
     } else {
-      $('.counter').removeClass('exceeded')
+      $('.counter').removeClass('exceeded');
     }
-   
   })
-  
 });
